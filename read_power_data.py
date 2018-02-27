7769585a96b48ec4d56c32669b0c5744b1c3cce0
Jeroen Delcour <jeroendelcour@gmail.com>
2018-02-27 22:38:27 +0100

    turned into live read-out script

--- read_power_data.py
+++ read_power_data.py
@@ -1,20 +1,20 @@
-from os.path import getsize
-from struct import iter_unpack
-import numpy as np
+from os import SEEK_END
+from struct import unpack
+from time import sleep
 
 BATTERY_LOG = './data/battery'
 SOLAR_LOG = './data/solar'
 
-def read(path):
+def tail(path):
     with open(path, 'rb') as f:
-        return [x for x in iter_unpack('<Lff', f.read())]
+        f.seek(-12, SEEK_END) # last 12 bytes
+        return unpack('<Lff', f.read())
 
 def main():
-    print('datetime (epoch) - voltage (V) - current (mA)')
-    print('Battery')
-    print(read(BATTERY_LOG))
-    print('Solar')
-    print(read(SOLAR_LOG))
+    while True:
+        datetime, voltage, current = tail(BATTERY_LOG)
+        print('{:.3f}V {:.1f}mA'.format(voltage, current))
+        sleep(1)
 
 if __name__ == '__main__':
     main()
