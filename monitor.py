b017d2d0b9db49a9a39155af2a8dd105a2e528bc
Jeroen Delcour <jeroendelcour@gmail.com>
2018-05-12 10:46:28 +0200

    Pi will now shut down when battery voltage drops below safe minimum (currently set to 3)

--- monitor.py
+++ monitor.py
@@ -3,12 +3,14 @@ from ina219 import INA219, DeviceRangeError
 from struct import pack
 from time import time, sleep
 from psutil import cpu_percent, virtual_memory
+from os import system
 
 SHUNT_OHMS = 0.1
 #MAX_EXPECTED_AMPS = 0.4
 BATTERY_LOG = '/home/pi/solarpi/data/battery.data'
 SOLAR_LOG = '/home/pi/solarpi/data/solar.data'
 SYSTEM_LOG = '/home/pi/solarpi/data/system.data'
+MIN_BATTERY_VOLTAGE = 3.0
 
 def save_line(path, x, y):
     with open(path, 'ab') as f:
@@ -20,11 +22,13 @@ def main():
     solar = INA219(SHUNT_OHMS, address=0x40)
     battery.configure(battery.RANGE_16V)
     solar.configure(solar.RANGE_16V)
+    shutting_down = False
 
     while True:
         battery.wake()
         try:
-            save_line(BATTERY_LOG, battery.voltage(), battery.current()) 
+            battery_voltage = battery.voltage()
+            save_line(BATTERY_LOG, battery_voltage, battery.current()) 
         except DeviceRangeError as e:
             print(e)
         battery.sleep()
@@ -37,6 +41,11 @@ def main():
         solar.sleep()
 
         save_line(SYSTEM_LOG, cpu_percent(), virtual_memory().percent)
+
+        if not shutting_down and battery_voltage < MIN_BATTERY_VOLTAGE:
+            print('Battery voltage ({}) below safe minimum ({})! Shutting down soon...'.format(battery_voltage, MIN_BATTERY_VOLTAGE))
+            system("sudo shutdown -r +5 'Battery voltage below safe minimum! Shutting down soon.'")
+            shutting_down = True
 
         sleep(1)
 
